0f0acee846f5c7023ad4d787976716cab50a8634
Mulanga Sibeli <mulanga.sibeli1@gmail.com>
2024-11-28 18:05:42 +0200

    Added components for UI

---
+++ ai-canvas/components/Button.tsx
@@ -0,0 +1,84 @@
+import { Pressable, StyleSheet, ViewProps } from "react-native";
+
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { ThemedText } from "../../../ai-canvas/ai-canvas/components/ThemedText";
+
+export type ButtonProps = ViewProps & {
+  lightColor?: string;
+  darkColor?: string;
+  lightTextColor?: string;
+  darkTextColor?: string;
+  title: string;
+  type?: "regular" | "outline" | "text";
+  icon?: React.ReactNode;
+  onPress: () => void;
+};
+
+export function Button({
+  style,
+  lightColor,
+  darkColor,
+  lightTextColor,
+  darkTextColor,
+  title,
+  type = "regular",
+  icon,
+  onPress,
+  ...rest
+}: ButtonProps) {
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "background"
+  );
+
+  const borderColor = useThemeColor(
+    { light: lightTextColor, dark: darkTextColor },
+    "text"
+  );
+
+  return (
+    <Pressable
+      style={[
+        styles.button,
+        { backgroundColor },
+        type === "regular" ? { backgroundColor: backgroundColor } : undefined,
+        type === "outline"
+          ? {
+              backgroundColor: "transparent",
+              borderWidth: 1,
+              borderColor: borderColor,
+            }
+          : undefined,
+        type === "text" ? [] : undefined,
+        style,
+      ]}
+      {...rest}
+      onPressOut={onPress}
+    >
+      <ThemedText
+        type="Caption"
+        weight="Regular"
+        lightColor={lightTextColor}
+        darkColor={darkTextColor}
+      >
+        {title}
+      </ThemedText>
+      {icon && <>{icon}</>}
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 5,
+    borderRadius: 10,
+    alignItems: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 2,
+    justifyContent: "center",
+    width: "auto",
+    height: "auto",
+  },
+});
---
+++ ai-canvas/components/ThemedInput.tsx
@@ -0,0 +1,141 @@
+import { StyleSheet, type ViewProps } from "react-native";
+import { Dispatch, SetStateAction } from "react";
+import { ThemedView } from "./ThemedView";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { ThemedText } from "./ThemedText";
+import { TextInput } from "react-native-paper";
+
+export type ThemedInputProps = ViewProps & {
+  lightColor?: string;
+  darkColor?: string;
+  value?: string;
+  placeholder?: string;
+  countryCode?: string;
+  inputLabel?: string;
+  inputLabelType?: "Caption" | "Regular" | "Subheading";
+  inputLabelColor?: string;
+  setValue?: Dispatch<SetStateAction<string>>;
+  setCountryCode?: Dispatch<SetStateAction<string>>;
+  showPassword?: boolean;
+  toggleShowPassword?: () => void;
+  submitAction?: () => void;
+  icon?: React.ReactNode;
+  secureInput?: boolean;
+  type: "regular" | "password" | "phonenum" | "email";
+};
+
+export function ThemedInput({
+  lightColor = "royalblue",
+  darkColor,
+  value,
+  placeholder,
+  countryCode,
+  inputLabel,
+  inputLabelType,
+  inputLabelColor,
+  setValue,
+  setCountryCode,
+  showPassword = true,
+  toggleShowPassword,
+  submitAction,
+  secureInput,
+  icon,
+  type,
+  ...otherProps
+}: ThemedInputProps) {
+  return (
+    <ThemedView style={styles.inputLayout}>
+      <ThemedView style={styles.inputsContainer}>
+        {type == "phonenum" && (
+          <ThemedView style={styles.phoneNumberInput}>
+            <ThemedView style={styles.codeInput}>
+              <TextInput
+                secureTextEntry={!showPassword}
+                onChangeText={setValue}
+                value={countryCode}
+                style={styles.codeInputContent}
+                placeholder={countryCode}
+              />
+            </ThemedView>
+            <ThemedView style={styles.phoneNumberInputDivider} />
+          </ThemedView>
+        )}
+        <TextInput
+          secureTextEntry={!showPassword}
+          style={[styles.input]}
+          onChangeText={setValue}
+          value={value}
+          placeholder={placeholder}
+          label={inputLabel}
+          placeholderTextColor={"#aaa"}
+          selectionColor={"black"}
+          onSubmitEditing={submitAction}
+          mode="outlined"
+          outlineColor="gray"
+          activeOutlineColor="royalblue"
+          outlineStyle={{ borderWidth: 1.5, backgroundColor: "transparent" }}
+          dense={true}
+          contentStyle={{ fontFamily: "Poppins-Regular" }}
+        />
+        {type == "password" ? (
+          <MaterialCommunityIcons
+            name={showPassword ? "eye-off" : "eye"}
+            size={18}
+            color="#aaa"
+            style={styles.passwordIcon}
+            onPress={toggleShowPassword}
+          />
+        ) : (
+          icon && <>{icon}</>
+        )}
+      </ThemedView>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputLayout: {
+    gap: 5,
+    backgroundColor: "transparent",
+  },
+  inputsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "transparent",
+  },
+  phoneNumberInput: {
+    height: "100%",
+    width: "15%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  codeInput: {
+    width: "90%",
+    paddingHorizontal: 10,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  codeInputContent: {
+    fontSize: 10,
+    fontFamily: "Poppins-Regular",
+  },
+  input: {
+    fontSize: 13,
+    lineHeight: 16,
+    height: 40,
+    fontFamily: "Poppins-Regular",
+    backgroundColor: "transparent",
+    width: "100%",
+  },
+  phoneNumberInputDivider: {
+    width: 1,
+    backgroundColor: "lightgray",
+    height: "60%",
+  },
+  passwordIcon: {
+    marginLeft: 10,
+  },
+});
